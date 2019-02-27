218e7fec11325f4d1427e2cbe41cdf94ba5d9450
zou-song <zousong0322@qq.com>
2019-02-27 22:57:14 +0800

    lc13: problem solved

--- header/lc.h
+++ header/lc.h
@@ -1,12 +1,14 @@
 #ifndef __LEETCODE_LC_H__
 #define __LEETCODE_LC_H__
 
+#include <algorithm>
+#include <assert.h>
 #include <iostream>
-#include <vector>
+#include <numeric>
 #include <sstream>
 #include <string>
-#include <algorithm>
-#include <numeric>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
@@ -53,6 +55,32 @@ string integerVectorToString(vector<int> list, int length = -1) {
         result += to_string(number) + ", ";
     }
     return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+string stringToString(string input) {
+    assert(input.length() >= 2);
+    string result;
+    for (size_t i = 1; i < input.length() -1; i++) {
+        char currentChar = input[i];
+        if (input[i] == '\\') {
+            char nextChar = input[i+1];
+            switch (nextChar) {
+                case '\"': result.push_back('\"'); break;
+                case '/' : result.push_back('/'); break;
+                case '\\': result.push_back('\\'); break;
+                case 'b' : result.push_back('\b'); break;
+                case 'f' : result.push_back('\f'); break;
+                case 'r' : result.push_back('\r'); break;
+                case 'n' : result.push_back('\n'); break;
+                case 't' : result.push_back('\t'); break;
+                default: break;
+            }
+            i++;
+        } else {
+          result.push_back(currentChar);
+        }
+    }
+    return result;
 }
 
 #endif

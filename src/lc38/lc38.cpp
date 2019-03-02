64409e3e487d642b3563ca2ad041ad10cf63b43d
zous <zousong0322@qq.com>
2019-03-02 17:44:58 +0800

    lc38: problem unsolved

---
+++ src/lc38/lc38.cpp
@@ -0,0 +1,41 @@
+#include "lc.h"
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        string str = "1";
+        if (n <= 1)
+            return str;
+        for (int i = 2; i <= n; ++i)
+        {
+            int len = str.size();
+            string tmp;
+            int j = 0, k = 0;
+            while (k < len)
+            {
+                while (k < len && str[k] == str[j])
+                {
+                    k++;
+                }
+                int cnt = k - j;
+                tmp = tmp + to_string(cnt) + to_string(str[j]);
+                j = k;
+            }
+            str = tmp;
+        }
+        return str;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    string line;
+    while (getline(cin, line)) {
+        int n = stringToInteger(line);
+        
+        string ret = Solution().countAndSay(n);
+
+        string out = (ret);
+        cout << out << endl;
+    }
+    return 0;
+}

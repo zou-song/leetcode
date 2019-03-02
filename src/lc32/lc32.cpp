f61806f2726efdc17505d7570e2f2fa1674e5fea
zous <zousong0322@qq.com>
2019-03-02 14:13:16 +0800

    lc32: problem unsolved

---
+++ src/lc32/lc32.cpp
@@ -0,0 +1,45 @@
+#include "lc.h"
+
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        if (s.empty())
+            return 0;
+        int len = s.size();
+        vector<int> tmp(len);
+        tmp[0] = 0;
+        for (int i = 1; i < len; ++i)
+        {
+            if (s[i] == '(')
+                tmp[i] = 0;
+            else
+            {
+                int j = i - 1- tmp[i - 1];
+                if (s[j] == '(')
+                {
+                    if (j - 1 < 0)
+                        tmp[i] = 2 + tmp[i - 1];
+                    else
+                        tmp[i] = 2 + tmp[i - 1] + tmp[j - 1];
+                }
+                else
+                    tmp[i] = 0;
+            }
+        }
+        return *max_element(tmp.begin(), tmp.end());
+    }
+};
+
+int main(int argc, char *argv[])
+{
+	string line;
+    while (getline(cin, line)) {
+        string s = stringToString(line);
+        
+        int ret = Solution().longestValidParentheses(s);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+	return 0;
+}

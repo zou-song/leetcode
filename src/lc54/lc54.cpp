52d982f025c5606bc4ee699b6c86976505b47e8d
zous <zousong0322@qq.com>
2019-03-04 20:01:43 +0800

    lc54: problem solved

--- src/lc54/lc54.cpp
+++ src/lc54/lc54.cpp
@@ -6,19 +6,33 @@ public:
         if (matrix.empty())
             return {};
         vector<int> ret;
-        size_t rows = matrix.size();
-        size_t cols = matrix[0].size();
-        for (size_t i = 0; i < rows / 2 && i < cols / 2; ++i)
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        int i = 0;
+        for ( ; i < rows / 2 && i < cols / 2; ++i)
         {
-            size_t rc = cols - i - 1;
-            for (size_t j = i; j <= rc; ++j)
+            int rc = cols - i - 1;
+            for (int j = i; j <= rc; ++j)
                 ret.push_back(matrix[i][j]);
-            size_t br = rows - i - 1;
-            for (size_t j = i + 1; j <= br; ++j)
+            int br = rows - i - 1;
+            for (int j = i + 1; j <= br; ++j)
                 ret.push_back(matrix[j][rc]);
-            for (size_t j = rc - 1; j >= i; --j)
+            for (int j = rc - 1; j >= i && br != i; --j)
                 ret.push_back(matrix[br][j]);
-            for (size_t j = br - 1; j > i; --j)
+            for (int j = br - 1; j > i && rc != i; --j)
+                ret.push_back(matrix[j][i]);
+        }
+        if (2 * i < rows && 2 * i < cols)
+        {
+            int rc = cols - i - 1;
+            for (int j = i; j <= rc; ++j)
+                ret.push_back(matrix[i][j]);
+            int br = rows - i - 1;
+            for (int j = i + 1; j <= br; ++j)
+                ret.push_back(matrix[j][rc]);
+            for (int j = rc - 1; j >= i && br != i; --j)
+                ret.push_back(matrix[br][j]);
+            for (int j = br - 1; j > i && rc != i; --j)
                 ret.push_back(matrix[j][i]);
         }
         return ret;

5d6060ba085b55311208b6ae5a48ba9e01283139
zous <zousong0322@qq.com>
2019-03-05 12:27:42 +0800

    lc63: problem solved

--- src/lc63/lc63.cpp
+++ src/lc63/lc63.cpp
@@ -6,8 +6,8 @@ public:
         if (obstacleGrid.empty())
             return 0;
         int rows = obstacleGrid.size(), cols = obstacleGrid[0].size();
-        vector<vector<int>> flag(rows);
-        for_each(flag.begin(), flag.end(), [cols](vector<int> &v){
+        vector<vector<unsigned int>> flag(rows);
+        for_each(flag.begin(), flag.end(), [cols](vector<unsigned int> &v){
             v.resize(cols);
         });
         flag[rows - 1][cols - 1] = (obstacleGrid[rows - 1][cols - 1] == 1) ? 0 : 1;
@@ -16,7 +16,7 @@ public:
             for (int j = cols - 1; j >= 0; --j)
             {
                 if (i == rows - 1 && j == cols - 1) continue;
-                int v1 = 0, v2 = 0;
+                unsigned int v1 = 0, v2 = 0;
                 if (j + 1 < cols)
                     v1 = flag[i][j + 1];
                 if (i + 1 < rows)

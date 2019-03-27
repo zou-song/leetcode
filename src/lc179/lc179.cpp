b2bf00eef892bd3fcbf504d77ec0494c44787d0d
zous <zousong0322@qq.com>
2019-03-27 16:56:38 +0800

    lc179: problem solved

--- src/lc179/lc179.cpp
+++ src/lc179/lc179.cpp
@@ -7,39 +7,14 @@ public:
         for (auto n : nums)
             strs.push_back(to_string(n));
         sort(strs.begin(), strs.end(), [](const string& s1, const string& s2){
-            int len1 = s1.size();
-            int len2 = s2.size();
-            int idx1 = 0, idx2 = 0;
-            while (idx1 < len1 && idx2 < len2)
-            {
-                if (s1[idx1] > s2[idx2])
-                    return true;
-                else if (s1[idx1] < s2[idx2])
-                    return false;
-                else
-                {
-                    idx1++;
-                    idx2++;
-                }
-            }
-            if (idx1 < len1)
-            {
-                if (s1[idx1] > s1[0])
-                    return true;
-                return false;
-            }
-            if (idx2 < len2)
-            {
-                if (s2[idx2] > s2[0])
-                    return false;
-            }
-            return true;
+            return s1 + s2 > s2 + s1;
         });
         string ret;
         for_each(strs.begin(), strs.end(), [&ret](const string& s){
-            ret = ret + s;
+            if (ret != "0" || s != "0")
+                ret = ret + s;
         });
-        return ret;
+        return ret.empty() ? "0" : ret;
     }
 };
 

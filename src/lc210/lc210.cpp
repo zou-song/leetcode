ec63efeaf28375e6bd00dd0c3afee08c640d5831
zous <zousong0322@qq.com>
2019-04-02 15:04:46 +0800

    lc210: problem solved

--- src/lc210/lc210.cpp
+++ src/lc210/lc210.cpp
@@ -54,7 +54,7 @@ int main(int argc, char const *argv[])
 		int numCourses = 0;
 		walkString(numCourses, line);
 		getline(cin, line);
-		vector<int> prerequisites;
+		vector<pair<int, int>> prerequisites;
 		walkString(prerequisites, line);
 		cout << toString(Solution().findOrder(numCourses, prerequisites)) << endl;
 	}

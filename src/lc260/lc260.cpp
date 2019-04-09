5896eb50126eb241c6d34184e8bc7fb9a45d1c85
zous <zousong0322@qq.com>
2019-04-09 17:25:28 +0800

    lc260: problem solved

---
+++ src/lc260/lc260.cpp
@@ -0,0 +1,30 @@
+#include "lc.h"
+
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int tmp = 0;
+        for (int n : nums)
+            tmp ^= n;
+        int x = (tmp ^ (tmp - 1) + 1) >> 2;
+        int n1 = tmp;
+        for (int n : nums)
+            if (n & x)
+                n1 ^= n;
+        int n2 = tmp ^ n1;
+        vector<int> ret = {n1, n2};
+        return ret;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+	string line;
+	while (getline(cin, line))
+	{
+		vector<int> nums;
+		walkString(nums, line);
+		cout << toString(Solution().singleNumber(nums)) << endl;
+	}
+	return 0;
+}

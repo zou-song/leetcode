c80cb5a06f8095d614a35bfdaba6caa4e5ffb986
zous <zousong0322@qq.com>
2019-04-13 17:34:16 +0800

    lc279: problem unsolved

---
+++ src/lc279/lc279.cpp
@@ -0,0 +1,40 @@
+#include "lc.h"
+
+class Solution {
+public:
+    int numSquares(int n) {
+        static unordered_map<int, int> solved_map;
+        auto iter = solved_map.find(n);
+        if (iter != solved_map.end())
+            return iter->second;
+        int q = sqrt(n);
+        if (q * q == n)
+        {
+            solved_map[n] == 1;
+            return 1;
+        }
+        int ret = n;
+        for (int i = q; i >= 1; --i)
+        {
+            int k = numSquares(n - q * q);
+            if (ret > 1 + k)
+            {
+                ret = 1 + k;
+            }
+        }
+        solved_map[n] = ret;
+        return ret;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+	string line;
+	while (getline(cin, line))
+	{
+		int n = 0;
+		walkString(n, line);
+		cout << toString(Solution().numSquares(n)) << endl;
+	}
+	return 0;
+}

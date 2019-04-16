a1b60736a60dd4046663d7c6b74f7020072d4297
zous <zousong0322@qq.com>
2019-04-16 13:44:55 +0800

    lc292: problem solved

---
+++ src/lc292/lc292.cpp
@@ -0,0 +1,20 @@
+#include "lc.h"
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        return n % 4;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+	strinr line;
+	while (getline(cin, line))
+	{
+		int n = 0;
+		walkString(n, line);
+		cout << toString(Solution().canWinNim(n)) << endl;
+	}
+	return 0;
+}

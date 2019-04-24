7a959a00ed45d15367571096b4b82d04c8fa2b5e
zous <zousong0322@qq.com>
2019-04-24 15:04:02 +0800

    lc87: problem solved

--- src/lc87/lc87.cpp
+++ src/lc87/lc87.cpp
@@ -2,31 +2,55 @@
 
 class Solution {
 public:
-    bool isScramble(const string& s1, int b1, int e1, const string& s2, int b2, int e2)
-    {
-        if (e1 - b1 != e2 - b2)
-            return false;
-        else if (e1 < b1)    return true;
-        else if (s1.substr(b1, e1 - b1 + 1) == s2.substr(b2, e2 - b2 + 1))
-            return true;
-        for (int i = b2; i <= e2; ++i)
-        {
-            if (s2[i] == s1[b1])
-            {
-                int len = i - b2 + 1;
-                bool ret = isScramble(s1, b1 + 1, b1 + len - 1, s2, b2, i - 1) &&
-                    isScramble(s1, b1 + len, e1, s2, i + 1, e2);
-                if (ret)
-                    return ret;
-                else    continue;
-            }
-        }
-        return false;
-    }
     bool isScramble(string s1, string s2) {
         int len1 = s1.size();
         int len2 = s2.size();
-        return isScramble(s1, 0, len1 - 1, s2, 0, len2 - 1);
+        if (len1 != len2)   return false;
+        vector<vector<vector<bool>>> dp(len1);
+        for_each(dp.begin(), dp.end(), [len1](vector<vector<bool>>& vec){
+            vec.resize(len1);
+            for_each(vec.begin(), vec.end(), [len1](vector<bool>& v){
+                v.resize(len1 + 1);
+            });
+        });
+        for (int i = 0; i < len1; ++i)
+        {
+            for (int j = 0; j < len1; ++j)
+            {
+                fill(dp[i][j].begin(), dp[i][j].end(), false);
+                dp[i][j][0] = true;
+                if (s1[i] == s2[j])
+                {
+                    dp[i][j][1] = true;
+                }
+            }
+        }
+        for (int len = 2; len <= len1; ++len)
+        {
+            for (int i = 0; i < len1; ++i)
+            {
+                for (int j = 0; j < len1; ++j)
+                {
+                    for (int k = 1; k < len; ++k)
+                    {
+                        if (j + k >= len1)  break;
+                        if (i + k >= len1)  break;
+                        if (j + len - k >= len1)    continue;
+                        if (dp[i][j][k] && dp[i + k][j + k][len - k])
+                        {
+                            dp[i][j][len] = true;
+                            break;
+                        }
+                        else if (dp[i][j + len - k][k] && dp[i + k][j][len - k])
+                        {
+                            dp[i][j][len] = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return dp[0][0][len1];
     }
 };
 

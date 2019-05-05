b35a82113974f89db028df4df25731bf146745c9
zous <zousong0322@qq.com>
2019-05-05 16:00:54 +0800

    lc220: problem unsolved

--- src/lc220/lc220.cpp
+++ src/lc220/lc220.cpp
@@ -1,139 +1,37 @@
 #include "lc.h"
 
 class Solution {
-    template<typename T, typename Comp>
-    struct PeakStack
-    {
-        stack<T> _elems;
-        stack<T> _peak;
-        
-        void push(const T& v)
-        {
-            _elems.push(v);
-            Comp cmp;
-            if (_peak.empty() || cmp(v, _peak.top()))
-            {
-                _peak.push(v);
-            }
-            else
-            {
-                _peak.push(_peak.top());
-            }
-        }
-        
-        void pop()
-        {
-            _elems.pop();
-            _peak.pop();
-        }
-        
-        size_t size() const
-        {
-            return _elems.size();
-        }
-        
-        bool empty() const
-        {
-            return _elems.empty();
-        }
-        
-        T top() const
-        {
-            return _elems.top();
-        }
-        
-        T peak() const
-        {
-            return _peak.top();
-        }
-    };
-    
-    template<typename T, typename Comp>
-    struct PeakQueue
-    {
-        PeakStack<T, Comp> _stk1;
-        PeakStack<T, Comp> _stk2;
-        
-        void push(const T& v)
-        {
-            _stk1.push(v);
-        }
-        
-        void pop()
-        {
-            if (!_stk2.empty())
-            {
-                _stk2.pop();
-            }
-            else
-            {
-                transferStack();
-                _stk1.pop();
-            }
-        }
-        
-        size_t size() const
-        {
-            return _stk1.size() + _stk2.size();
-        }
-        
-        T peak() const
-        {
-            if (_stk1.empty())
-            {
-                return _stk2.peak();
-            }
-            else if(_stk2.empty())
-            {
-                return _stk1.peak();
-            }
-            
-            T v1 = _stk1.peak();
-            T v2 = _stk2.peak();
-            
-            return Comp()(v1, v2) ? v1 : v2;
-        }
-        
-        void transferStack()
-        {
-            while (_stk1.size() > 1)
-            {
-                T v = _stk1.top();
-                _stk1.pop();
-                _stk2.push(v);
-            }
-        }
-    };
-    
 public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
         int len = nums.size();
         if (len <= 0)   return false;
-        PeakQueue<int, greater<int>> max_que;
-        PeakQueue<int, less<int>> min_que;
-        max_que.push(nums[0]);
-        min_que.push(nums[0]);
-        for (int i = 1; i < len; ++i)
+        if (k <= 0 || t <= 0) return false;
+        unordered_map<long long, long long> hashmap;
+        for (int i = 0; i < len; ++i)
         {
-            while (max_que.size() > (size_t)k)
+            long long n = (long long)nums[i] - INT_MIN;
+            long long idx = n / ((long long)t + 1);
+            if (hashmap.find(idx) != hashmap.end())
             {
-                max_que.pop();
+                return true;
             }
-            while (min_que.size() > (size_t)k)
+            auto iter1 = hashmap.find(idx - 1);
+            if (iter1 != hashmap.end() && n - iter1->second <= t)
             {
-                min_que.pop();
+                return true;
             }
-            long long max_v = max_que.peak();
-            long long min_v = min_que.peak();
-            long long n = nums[i];
-            if (abs(n - max_v) > t || abs(n - min_v) > t)
+            auto iter2 = hashmap.find(idx + 1);
+            if (iter2 != hashmap.end() && iter2->second - n <= t)
             {
-                return false;
+                return true;
             }
-            max_que.push(nums[i]);
-            min_que.push(nums[i]);
+            if (hashmap.size() >= k)
+            {
+                long long rmidx = ((long long)nums[i - k] - INT_MIN) / ((long long)t + 1);
+                hashmap.erase(rmidx);
+            }
         }
-        return true;
+        return false;
     }
 };
 

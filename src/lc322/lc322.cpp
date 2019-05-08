9b95b811e13afa27916d6cf64f5c7ba86e553e8f
zous <zousong0322@qq.com>
2019-05-08 16:20:48 +0800

    lc322: problem solved

--- src/lc322/lc322.cpp
+++ src/lc322/lc322.cpp
@@ -4,27 +4,18 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         vector<int> dp(amount + 1, 0);
-        sort(coins.begin(), coins.end());
         for (int i = 1; i <= amount; ++i)
         {
-            dp[i] = INT_MAX;
+            dp[i] = amount + 1;
             for (int ci : coins)
             {
-                if (i - ci < 0)
-                {
-                    break;
-                }
-                else if (dp[i - ci] == INT_MAX)
-                {
-                    dp[i] = INT_MAX;
-                }
-                else
+                if (i >= ci)
                 {
                     dp[i] = min(dp[i], dp[i - ci] + 1);
                 }
             }
         }
-        return (dp[amount] == INT_MAX) ? -1 : dp[amount];
+        return (dp[amount] > amount) ? -1 : dp[amount];
     }
 };
 

86db4496b8b6a81500f52400b9b85544e16130ed
zous <zousong0322@qq.com>
2019-05-16 15:21:57 +0800

    lc381: problem solved

--- src/lc381/lc381.cpp
+++ src/lc381/lc381.cpp
@@ -72,14 +72,6 @@ public:
  * int param_3 = obj->getRandom();
  */
 
-/**
- * Your RandomizedCollection object will be instantiated and called as such:
- * RandomizedCollection* obj = new RandomizedCollection();
- * bool param_1 = obj->insert(val);
- * bool param_2 = obj->remove(val);
- * int param_3 = obj->getRandom();
- */
-
  int main(int argc, char const *argv[])
  {
  	/*code*/
